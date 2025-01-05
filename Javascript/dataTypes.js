cf6c36326f0cc011652ebfc053891bd23028ea56
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-05 22:54:12 +0545

    new topics

---
+++ Javascript/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert("hello")  we are using nodejs not browser
+
+let name = "hitesh"
+let age = 23
+let isLoggedIn = false
+
+
+// number --> 2**53
+// bigint 
+// string -->  ""
+// boolean  --> true/false
+// null --> standalone value
+// undefined --> 
+// symbol --> unique
+
+//object
+
+console.log(typeof null); // it's a object and type of undefined is undefined
+
+
