d1258fbde845fde337fc5bdcecf64a946efb584e
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-06 16:44:36 +0545

    new

--- Javascript/conversionandoperation.js
+++ Javascript/conversionandoperation.js
@@ -1,8 +1,8 @@
-let score = "66abc"
+// let score = "66abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueinnumber = Number(score)
+// let valueinnumber = Number(score)
 // console.log(typeof valueinnumber);
 // console.log(valueinnumber);
 
@@ -12,15 +12,42 @@ let valueinnumber = Number(score)
 // true --> 1, false --> 0
 
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1 --> true 0 --> false
 // "" --> false
 // "risav" --> true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(someNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(someNumber);
+
+
+
+// operations
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**3);
+
+
+let str1 = "hello"
+let str2 = " risav"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+
+
+
